2cc2075769f1d83671190df43ac8e5b0ad9718e6
ecjz01 <ecjz88@gmail.com>
2021-12-11 17:56:38 -0500

    Jeff's Stuff

---
+++ cscproject.py
@@ -0,0 +1,33 @@
+"""
+This is my draft of the csc project. I want to essentially create the data scraper for all of the datasets
+
+requirements
+- datasets have to be all the same
+- if they are not same, then I have to do other stuff that will make me sad.
+"""
+
+from dataclasses import dataclass
+import csv
+
+
+@dataclass
+class Company:
+    """
+    Dataclass of relevant company information
+
+    Attributes
+    - market_cap: the market cap of the company pre covid
+    - revenue: the quarterly revenue of the company pre covid
+    - industry: the industry the company is in
+
+    Representation Invariants:
+      - self.market_cap >= 0
+      - self.revenue >= 0
+      - self.industry != ''
+
+    Sample Usage:
+    >>> pre_covid_apple = Company(market_cap = 87935000000, revenue = 247417000000, industry = 'Technology')
+    """
+
+def scrape_file(file: csvfile):
+

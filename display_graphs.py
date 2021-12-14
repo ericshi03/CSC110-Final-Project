1e757fa9c73aa8bc4eccaa9c58f7ea7db4b02add
ericshi03 <ericshi97@gmail.com>
2021-12-13 22:35:54 -0500

    main

--- display_graphs.py
+++ display_graphs.py
@@ -3,19 +3,17 @@ Copyright and Usage Information
 ===============================
 This file is Copyright (c) 2021 Danesh Kohina, Enfei Zhang, Eric Shi, Jefferson Liu
 
-TODO: ADD UNITS TO THE VALUES
-
 This file will take in data and use the plotly, statsmodel, and pandas modules to
 graphically represent the correlation between our calculated risk factor and a factor
 of our choosing.
 """
-import pandas
-import plotly.express as px
-import classes
 from typing import Optional
+from pandas import DataFrame
+import plotly.express as px
+from classes import Industry
 
 
-def convert_to_dataframe(values: list[classes.Industry]):
+def convert_to_dataframe(values: list[Industry]) -> DataFrame:
     """
     This function will convert the list of Industries into a dataframe class to ensure
     ease of use when working with plotly.
@@ -33,12 +31,13 @@ def convert_to_dataframe(values: list[classes.Industry]):
         expenses_avg.append(val.expenses[0])
         vuln_val.append(val.vulnerability)
     # Extra line for improved clarity
-    processed_values = {'Industries': names, 'Lay Off Percentages': lay_off_per_avg, 'Revenue': revenue_avg,
-                        'Expenses': expenses_avg, 'Vulnerability Values': vuln_val}
-    return pandas.DataFrame(data=processed_values)
+    processed_values = {'Industries': names, 'Lay Off Percentages': lay_off_per_avg,
+                        'Revenue': revenue_avg, 'Expenses': expenses_avg,
+                        'Vulnerability Values': vuln_val}
+    return DataFrame(data=processed_values)
 
 
-def display_linear_graphs(df: pandas.DataFrame, factor: Optional[str] = 'Expenses') -> None:
+def display_linear_graphs(df: DataFrame, factor: Optional[str] = 'Expenses') -> None:
     """
     This function will take in a DataFrame class and will output a graph based on the factor
     specified. This function will also perform a Linear Regression on the graph to determine
@@ -66,7 +65,8 @@ def display_linear_graphs(df: pandas.DataFrame, factor: Optional[str] = 'Expense
         shift = 0
 
     fig.add_annotation(x=shift, text='R<sup>2</sup> = '
-                                     + str(rfinder[0]) + '<br> Adjusted R<sup>2</sup> = ' + str(rfinder[1]),
+                                     + str(rfinder[0]) + '<br> Adjusted R<sup>2</sup> = '
+                                     + str(rfinder[1]),
                        showarrow=False,
                        xshift=1000,
                        yshift=-100)
@@ -90,7 +90,7 @@ if __name__ == '__main__':
 
     python_ta.check_all(config={
         'disable': ['R1729', 'C0412'],
-        'extra-imports': ['python_ta.contracts', 'hypothesis.strategies'],
+        'extra-imports': ['pandas', 'classes', 'plotly.express'],
         'max-line-length': 100
     })
 

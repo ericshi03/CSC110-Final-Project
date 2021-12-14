7a99f39026b9c62182085320a5a27619b953cc7e
ecjz01 <ecjz88@gmail.com>
2021-12-13 21:35:39 -0500

    Fixed Classes

--- classes.py
+++ classes.py
@@ -3,23 +3,24 @@ Copyright and Usage Information
 ===============================
 This file is Copyright (c) 2021 Danesh Kohina, Enfei Zhang, Eric Shi, Jefferson Liu
 """
-from dataclasses import dataclass
 import data as dt
 
 
-@dataclass
 class Industry:
-    """Dataclass of relevant industry information including layoff percentage, total layoffs, expenses and revenue
+    """A custom data type that represents industry information including layoff percentage, total layoffs, expenses
+    and revenue
+
     Instance Attributes:
       - name: the name of the industry
-      - layoffPercentages: the distributions of the percentage of people laid off in the industry
+      - lay_off_Percentages: the distributions of the percentage of people laid off in the industry
       - expenses: the distribution of the expenses of companies in the industry
       - revenue: the distribution of the revenue of companies in the industry
+
     Representation Invariants:
         - self.name != ''
-        - all(0 <= p <= 100 for p in self._layoffPercentages)
-        - all(0 <= p <= 100 for p in self._expenses)
-        - all(0 <= p <= 100 for p in self._revenue)
+        - all(0 <= p <= 100 for p in self.lay_off_Percentages)
+        - all(0 <= p <= 100 for p in self.expenses)
+        - all(0 <= p <= 100 for p in self.revenue)
     """
     name: str
     layoffPercentages: list[float]
@@ -29,30 +30,30 @@ class Industry:
 
     def __init__(self, ind: str) -> None:
         """
+        Initialize a new Industry object
         """
         self.name = ind
         key = ""
-        industries = ["Agriculture, forestry, fishing and hunting", "Mining, quarrying, and oil and gas extraction",
-                      "Construction", "Manufacturing", "Wholesale trade", "Retail trade",
-                      "Transportation and warehousing",
-                      "Information and cultural industries", "Finance and insurance",
-                      "Real estate and rental and leasing",
-                      "Professional, scientific and technical services", "Educational services",
-                      "Administrative and support, waste management and remediation services",
-                      "Health care and social assistance", "Arts, entertainment and recreation",
-                      "Accommodation and food services", "Other services except public administration"]
+        industries = ['Agriculture, forestry, fishing and hunting', 'Mining, quarrying, and oil and gas extraction',
+                      'Construction', 'Manufacturing', 'Wholesale trade', 'Retail trade',
+                      'Transportation and warehousing',
+                      'Information and cultural industries', 'Finance and insurance',
+                      'Real estate and rental and leasing',
+                      'Professional, scientific and technical services', 'Educational services',
+                      'Administrative and support, waste management and remediation services',
+                      'Health care and social assistance", "Arts, entertainment and recreation',
+                      'Accommodation and food services", "Other services except public administration']
         for industry in industries:
             if str.upper(self.name) in str.upper(industry):
                 key = industry
-        self.layoffPercentages = dt.read_industry_csv_file("3310025201-eng.csv")[key]
-        self.expenses = dt.read_industry_csv_file("3310028201-eng.csv")[key]
-        self.revenue = dt.read_industry_csv_file("3310028101-eng.csv")[key]
+        self.lay_off_Percentages = dt.read_industry_csv_file('3310025201-eng.csv')[key]
+        self.expenses = dt.read_industry_csv_file('3310028201-eng.csv')[key]
+        self.revenue = dt.read_industry_csv_file('3310028101-eng.csv')[key]
         self.vulnerability = self.calculate_industry_vulnerability()
 
     def calculate_industry_vulnerability(self) -> float:
-        """Calculates the vulnerability value of the industry
-
-
+        """
+        Returns the vulnerability value of the industry
         """
         average_layoff = 0
         for value in self.layoffPercentages:
@@ -68,21 +69,26 @@ class Industry:
 
 class Company:
     """
-    Dataclass of relevant company information
-    Attributes
+    A custom data type that represents company information including name, share price pre & post covid,
+    revenue pre & post covid and the industry it's in
+
+    Instance Attributes:
     - name: the name of the company
-    - share_price: the share price of the company pre covid
-    - revenue: the quarterly revenue of the company pre covid
+    - share_price_post_covid: the share price of the company post covid
+    - share_price_pre_covid: the share price of the company pre covid
+    - revenue_post_covid: the quarterly revenue of the company post covid
+    - revenue_pre_covid: the quarterly revenue of the company pre covid
     - industry: the industry the company is in
 
     Representation Invariants:
-      - self.name != ''
-      - self.share_price >= 0
-      - self.revenue >= 0
-      - self.industry.name != ''
-
+      - self._name != ''
+      - self._share_price_post_covid >= 0
+      - self._share_price_pre_covid >= 0
+      - self._revenue_post_covid >= 0
+      - self._revenue_pre_covid >= 0
+      - self._industry.name != ''
     Sample Usage:
-    >>> pre_covid_apple = Company(87935000000, 247417000000,  'Technology')
+    >>> apple = Company('Apple', 150.0, 100.0, 307417000000.0, 247417000000.0,  'Technology')
     """
     _name: str
     _share_price_post_covid: float
@@ -94,6 +100,7 @@ class Company:
     def __init__(self, name: str, share_post: float, share_pre: float, revenue_post: float, revenue_pre: float,
                  industry: str) -> None:
         """
+        Initialize a new Company object
         """
         self._name = name
         self._share_price_post_covid = share_post
@@ -103,7 +110,8 @@ class Company:
         self._industry = Industry(industry)
 
     def calculate_vulnerability_value(self) -> float:
-        """Calculates the vulnerability of the company
+        """
+        Returns the vulnerability of the company
         """
         total_mc = self._share_price_pre_covid + self._share_price_post_covid
         total_r = self._revenue_pre_covid + self._revenue_post_covid
